93b21538a8362c2c4d31bbd487a03c6d907e52b7
AliceLin321 <ataechiml24@gmail.com>
2022-11-07 13:53:11 -0500

    Alice Lin dom manipulation

---
+++ index.js
@@ -0,0 +1,6 @@
+const DOMselectors = {
+    title: document.getElementsById("title"),
+    artist: document.getElementById("artist"),
+    url:  document.getElementById ("url"),
+    display:  document.getElementById ("display")
+};
