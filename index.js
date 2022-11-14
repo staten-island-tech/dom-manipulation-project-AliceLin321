67b09edb4f5d049d2110d56fb8b3455f44bc3782
AliceLin321 <ataechiml24@gmail.com>
2022-11-14 13:57:03 -0500

    Alice Lin DOM Selector

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const DOMselectors = {
-  button: document.getElementsById("btn"),
-  text: document.getElementById("text"),
-  box: document.getElementById("BBB"),
-  points: document.getElementById(".point"),
+  form: document.getElementById("form"),
+  title: document.getElementById("title"),
+  artist: document.getElementById("artist"),
+  url: document.getElementById("url"),
+  display: document.getElementById("display"),
+  btn: document.getElementById("btn"),
 };
 function backgroundAndText(background, text) {
-    background.style.backgroundColor = "red";
-    text.textConten
+  background.style.backgroundColor = "red";
+  text.textContent = "bigger red box";
+  text.style.fontSize = "40px";
 }
+DOMselectors.btn.addEventListener("click", function () {
+  backgroundAndText(DOMselectors.box, DOMselectors.text);
+});
