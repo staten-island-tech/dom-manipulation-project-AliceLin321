9d3a05eed7e7f32ab981086507ecbfb88b5fab27
AliceLin321 <ataechiml24@gmail.com>
2022-11-16 13:47:51 -0500

    Alice Lin DOM MAnipulator

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const DOMselectors = {
   display: document.getElementById("display"),
   btn: document.getElementById("btn"),
 };
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.textContent = "bigger red box";
-  text.style.fontSize = "40px";
-}
-DOMselectors.btn.addEventListener("click", function () {
- function input() {
-  DOMselectors.display.insertAdjacentHTML("beforeend"),
-    (
-      <div class="display-card">
+DOMselectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  input();
+});
+function input() {
+  DOMselectors.display.insertAdjacentHTML(
+    "beforeend",
+
+    `<div class="display-card">
         <img class="display-img" src=${DOMselectors.url.value} />
         <h2 class="display-artist">${DOMselectors.artist.value}</h2>
         <h3 class="display-album">${DOMselectors.album.value}</h3>
         <button class="remove btn"></button>
-      </div>
-    );
-});
+      </div>`
+  );
+}
+console.log(yam);
